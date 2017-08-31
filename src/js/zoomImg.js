58340a8aafe8baf99ce966e165eca49eeb593058
sRect <fcqun@sina.cn>
2017-08-31 15:03:22 +0800

    设置默认下标为0

--- src/js/zoomImg.js
+++ src/js/zoomImg.js
@@ -6,7 +6,7 @@ var ZoomImg = (function(window) {
 	ZoomImg.fn = ZoomImg.prototype = {
 		constructor: ZoomImg,
 		init: function() {
-			this.indexNum = ""; //点击下标
+			this.indexNum = "" || 0; //点击下标
 			this.urlList = sessionStorage.getItem("urlList") || []; //图片数组
 
 			this.config = {
@@ -18,17 +18,17 @@ var ZoomImg = (function(window) {
 			this.getArguments = function(window) {
 				this._getArguments(window);
 			};
-			this.renderImg = function(){
+			this.renderImg = function() {
 				this._renderImg();
 			};
-			this.swiperFn = function(){
+			this.swiperFn = function() {
 				this._swiper();
 			};
-			this.bindEvent = function(window){
+			this.bindEvent = function(window) {
 				this._bindEvent(window);
 			};
 		},
-		_getArguments: function(window){ //获取点击下标
+		_getArguments: function(window) { //获取点击下标
 			var url = window.location.href;
 			var getParam = function(name) {
 				var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
@@ -40,47 +40,47 @@ var ZoomImg = (function(window) {
 			};
 
 			if (url.indexOf("index") != -1) {
-			    this.indexNum = getParam("index");
+				this.indexNum = getParam("index");
 			} else {
-			    $my.messageInfo.html("url错误").fadeIn("fast").delay("1000").fadeOut("slow");
-			    throw new Error("url错误");
-			}; 
+				$my.messageInfo.html("url错误").fadeIn("fast").delay("1000").fadeOut("slow");
+				throw new Error("url错误");
+			};
 		},
-		_renderImg: function(){
+		_renderImg: function() {
 			var str = "";
 			this.urlList = JSON.parse(this.urlList);
 			if (this.urlList.length) {
-				for (var i = 0,len = this.urlList.length; i < len; i++) {
+				for (var i = 0, len = this.urlList.length; i < len; i++) {
 					str += '<div class="swiper-slide">';
 					str += '<div class="swiper-zoom-container">';
-					str += '<img data-src='+this.urlList[i]+' class="swiper-lazy">';
+					str += '<img data-src=' + this.urlList[i] + ' class="swiper-lazy">';
 					str += '</div>';
 					str += '<div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>';
 					str += '</div>';
-				}; 
+				};
 				this.config.swiperWrapper.innerHTML = str;
-			} else{
+			} else {
 				$(this.config.messageInfo).html("图片为空").fadeIn("fast").delay("1000").fadeOut("slow");
 				return;
 			};
 		},
-		_swiper: function(){
+		_swiper: function() {
 			var swiper = new Swiper('.swiper-container', {
 				lazyLoading: true,
 				// Disable preloading of all images
 				preloadImages: false,
 				// Enable lazy loading
 				zoom: true,
-				zoomMax :4,
+				zoomMax: 4,
 				pagination: '.swiper-pagination',
 				// nextButton: '.swiper-button-next',
 				// prevButton: '.swiper-button-prev',
-				paginationType : 'fraction'
-		    });
-		    swiper.slideTo(this.indexNum, 1000, false);
+				paginationType: 'fraction'
+			});
+			swiper.slideTo(this.indexNum, 1000, false);
 		},
-		_bindEvent: function(window){
-			this.config.backBtn.addEventListener("touchend", function(event){
+		_bindEvent: function(window) {
+			this.config.backBtn.addEventListener("touchend", function(event) {
 				event.stopPropagation();
 				event.preventDefault();
 
@@ -102,5 +102,5 @@ $(function() {
 	zoomImg.renderImg(); //render img
 	zoomImg.swiperFn(); //swiper实例
 	zoomImg.bindEvent(window); //返回上一页
-	
+
 });
